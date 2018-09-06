d976a316b2becaa070723903d12804f66d72b973
Dmitry Lozychenko <Dimon5676@yandex.ru>
2018-09-07 00:25:29 +0300

    Added some improvements

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,14 @@ public class Main{
     public static void main(String[] args) {
         PList list = new PList();
         for (int i = 0; i <= 10; i++) {
-            list.add_to_end(i);
+            list.addToEnd(i);
         }
-        list.remove_from_begin();
-        list.remove_from_n(3);
-        list.remove_from_end();
-        list.add_to_end(999);
-        list.add_to_n(1, 3123123);
+        list.show();
+        list.removeFromBegin();
+        list.removeFromN(3);
+        list.removeFromEnd();
+        list.addToEnd(999);
+        list.addToN(1, 3123123);
         list.move(8, 2);
         list.show();
     }
--- src/main/java/PList.java
+++ src/main/java/PList.java
@@ -4,33 +4,82 @@ import java.util.List;
 public class PList {
     List<Integer> list = new ArrayList();
 
-    void add_to_begin(int i) {
+    /**
+     *
+     * @param i element to add in the beginning of the list
+     */
+    void addToBegin(int i) {
         list.add(0, i);
+        System.out.println("Element " + i + " is added to the beginning");
     }
-    void add_to_n(int n, int i) {
+    /**
+     *
+     * @param n index in which element will be inserted
+     * @param i element to insert
+     */
+    void addToN(int n, int i) {
         list.add(n, i);
+        System.out.println("Element " + i + " is added at " + n + " position");
     }
-    void add_to_end(int i) {
+
+    /**
+     *
+     * @param i element to add in the end of the list
+     */
+    void addToEnd(int i) {
         list.add(i);
+        System.out.println("Element " + i + " is added in the ending of the list");
     }
+
+    /**
+     *
+     * @param i element to find in the list
+     */
     void find(int i) {
         if (list.contains(i)) {
             System.out.println("Found: " + i );
         }
     }
-    void remove_from_begin() {
+
+    /**
+     * Removes the first element of the list
+     */
+    void removeFromBegin() {
         list.remove(0);
+        System.out.println("First element of the list has been removed");
     }
-    void remove_from_n(int n) {
+
+    /**
+     *
+     * @param n index of element which will be removed
+     */
+    void removeFromN(int n) {
         list.remove(n);
+        System.out.println("Element with index " + n + " has been removed");
     }
-    void remove_from_end () {
+
+    /**
+     * Removes last element from the list
+     */
+    void removeFromEnd () {
         list.remove(list.size()-1);
+        System.out.println("Last element of the list has been removed");
     }
+
+    /**
+     * Moves element with index 'k' back on 'n' positions
+     * @param k index of element
+     * @param n quantity of positions
+     */
     void move(int k, int n) {
-        add_to_n(k-n, k);
-        remove_from_n(k);
+        addToN(k-n, k);
+        removeFromN(k);
+        System.out.println("Element with index " + k + " has been moved back on " + n + " positions");
     }
+
+    /**
+     * Displays all elements of the list
+     */
     void show() {
         for (int i:list) {
             System.out.println(i);
