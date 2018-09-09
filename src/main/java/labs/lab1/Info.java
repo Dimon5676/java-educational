ec294a43d4cf7bee3740bf017752b0e6fe1a7f45
Dmitry Lozychenko <Dimon5676@yandex.ru>
2018-09-09 14:58:26 +0300

    Added comments

--- src/main/java/labs/lab1/Info.java
+++ src/main/java/labs/lab1/Info.java
@@ -2,20 +2,40 @@ package labs.lab1;
 
 import java.util.Date;
 
-public class Info {
+/**
+ * Test class for the first lab of programming course
+ * in Sevastopol State University
+ */
 
+public class Info {
+    /**
+     * Main method
+     * @param args array of args
+     */
     public static void main (String[] args) {
+        /** name of the student */
         String firstName = "Дмитрий";
+        /** middle name of the student */
         String middleName = "Романович";
+        /** last name of the student */
         String lastName = "Лозыченко";
+        /** first name of the teacher */
         String teacherFirstName = "Елена";
+        /** middle name of the teacher */
         String teacherMiddleName = "Сергеевна";
+        /** last name of the teacher */
         String teacherLastName = "Владимирова";
+        /** code of the student's group */
         String groupCode = "ИВТ/б-11-о";
+        /** number of student's variant */
         int variant = 12;
+        /** length of the pool */
         int longPool = 341;
+        /** width of the pool */
         int widthPool = 91;
+        /** full name of teacher or student */
         String fullName = fullName(firstName, middleName, lastName);
+        /** square of pool */
         int result = longPool * widthPool;
         System.out.println("Программу выполнил:");
         System.out.println(fullName);
@@ -29,7 +49,14 @@ public class Info {
     }
 
 
-
+    /**
+     * Returns full name
+     *
+     * @param a first name
+     * @param b middle name
+     * @param c last name
+     * @return full name
+     */
     public static String fullName(String a, String b, String c){
 
       return a + " " + b + " " + c;
--- src/main/java/labs/lab2/Format.java
+++ src/main/java/labs/lab2/Format.java
@@ -1,13 +1,28 @@
 package labs.lab2;
 
+/**
+ * Test class for the second lab of programming course
+ * in Sevastopol State University
+ */
 public class Format {
+    /**
+     * Main method
+     * @param args array of args
+     */
     public static void main(String[] args) {
+        /** last name and group code of student */
         String str = "Лозыченко ИВТ/б-11-о:";
+        /** integer variable */
         int a = 10425;
+        /** short variable */
         short b = 26;
+        /** byte variable */
         byte c = 11;
+        /** double variable */
         double d = 43.16542;
+        /** float variable */
         float f = 412.4322f;
+        /** Prints formatted string */
         System.out.printf("Привет, %s a=%x, b=%5d, c=%o, d=%+5.3f, f=%07.2f!\n", str, a, b, c, d, f);
     }
 }
